314cd2914c073c9f4d018d4617766618ad81e36c
DESKTOP-DP7DLDE\julio <juliogd2004@gmail.com>
2024-02-22 10:55:31 +0100

    Añadir controlador de categorías y rutas relacionadas

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return view('categories.index');
+    }
+
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->user_id = auth()->id();
+        $category->save();
+
+        return redirect()->route('categories.index');
+    }
+
+    public function edit(Category $category)
+    {
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $category->name = $request->name;
+        $category->save();
+
+        return redirect()->route('categories.index');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index');
+    }
+}
